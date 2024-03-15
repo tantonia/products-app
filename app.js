7725a39b6c4040c90a02e2e85c3fa49532c8d645
A. Tsatsouli <tantonia01@gmail.com>
2024-03-15 20:58:05 +0200

    swagger documentation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 
 app.use(express.json());
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger')
+
 mongoose.connect(process.env.MONGODB_URI).then(
     () => {console.log("Connection to mongodb established")},
     err => {console.log("Failed to connect to mongo db", err)}
@@ -15,6 +18,11 @@ const userProduct = require('./routes/user.products.routes')
 
 app.use('/api/users', user);
 app.use('/api/user-products', userProduct)
+
+app.use('/api-docs', 
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerDocument.options)
+)
 
 app.listen(port, () => {
     console.log("Server is up");
